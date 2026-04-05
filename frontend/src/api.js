bfc714ae6884a056793b07b57266b4f56d74d3a5
小狼爱吃小羊 <1280045538@qq.com>
2026-04-05 15:50:15 +0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://liujinyang-1.onrender.com'
+  baseURL: process.env.REACT_APP_API_URL || 'https://liujinyang-1.onrender.com/api'
 });
 
 export const setToken = (token) => {
