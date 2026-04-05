f194fc7c6aaa35147e87dc7e0246dfb89357f300
小狼爱吃小羊 <1280045538@qq.com>
2026-04-05 15:28:25 +0800

    Create GroupChat.js

---
+++ frontend/src/components/GroupChat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GroupChat() {
+  return (
+    <div>
+      <h2>Group Chat</h2>
+      <p>This is the group chat page.</p>
+    </div>
+  );
+}
+
+export default GroupChat;
+
